f3ad40ba8c78645cab507a67d9b74603e2e5a9ff
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-10-14 12:13:00 +0200

    chore: add `<TaskForm>` event handlers

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsClipboardPlus } from "react-icons/bs";
 import "./TaskForm.css";
 
-function TaskForm(props) {
+function TaskForm({ /** @type Function */ onSubmit }) {
+    const [input, setInput] = useState("");
+
+    const handleInputChange = (/** @type Event */ event) => {
+        setInput(event.target.value);
+    };
+
+    const handleFormSubmit = (/** @type Event */ event) => {
+        event.preventDefault();
+        onSubmit &&
+            onSubmit({
+                text: input,
+                completed: false,
+            });
+        setInput("");
+    };
+
     return (
-        <form className="task-form">
+        <form className="task-form" onSubmit={handleFormSubmit}>
             <input
                 type="text"
                 name="text"
                 className="task-input"
                 placeholder="Escribe una tarea"
                 autoComplete="off"
+                value={input}
+                required={true}
+                onChange={handleInputChange}
             />
             <button className="task-btn" aria-label="Agregar tarea">
                 <BsClipboardPlus />
