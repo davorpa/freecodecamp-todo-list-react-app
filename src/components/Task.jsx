4cb98096528b2595b375d1f04a2346a85d221f1a
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-10-14 11:21:33 +0200

    chore: add Task event handlers

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,18 +3,34 @@ import "./Task.css";
 import { BsTrash } from "react-icons/bs";
 
 function Task({
+    /** @type string */ id,
     /** @type string */ className,
     /** @type string */ text,
     /** @type boolean */ completed,
+    /** @type Function */ onComplete,
+    /** @type Function */ onDelete,
 }) {
     let cssClass = "task";
     className && (cssClass += " " + className);
+    onComplete && (cssClass += " completable");
+    onDelete && (cssClass += " deletable");
     completed && (cssClass += " completed");
+
+    const handleComplete = () => onComplete && onComplete(id);
+
+    const handleDelete = () => onDelete && onDelete(id);
+
     return (
-        <div className={cssClass}>
-            <div className="task-text">{text}</div>
+        <div className={cssClass} data-id={id}>
+            <div className="task-text" onClick={handleComplete}>
+                {text}
+            </div>
             <div className="task-actions">
-                <button className="btn" aria-label="Eliminar">
+                <button
+                    className="btn"
+                    aria-label="Eliminar"
+                    onClick={handleDelete}
+                >
                     <BsTrash />
                 </button>
             </div>
